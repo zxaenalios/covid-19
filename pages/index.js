0e1eb31572fb114963dc5787541d98c307fbdc10
Ali Imron <zxaenalios@gmail.com>
2021-10-09 19:31:05 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,9 @@ import { LayoutDashboard } from '../components';
 
 export default function Home() {
   return (
-    <LayoutDashboard/>
+    <LayoutDashboard title="Dashboard Covid-19">
+      <h1>Selamat Datang di Dashboard Covid-19</h1>
+    </LayoutDashboard>
    
   );
 }
