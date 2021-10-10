f2c0f51b1d1ca214f1dd14dacddd244f6aebc0ec
Ali Imron <zxaenalios@gmail.com>
2021-10-10 09:01:44 +0700

    carousel

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,12 @@ import { LayoutDashboard } from '../components';
 import { Carousel } from 'antd';
 
 const contentStyle = {
-  height: '400px',
+  height: 'auto',
   width: '100%',
   color: '#fff',
   textAlign: 'center',
   background: '#364d79',
-  borderRadius: '5px',
+  borderRadius: '10px'
 };
 
 export default function Home() {
@@ -16,7 +16,10 @@ export default function Home() {
       <h1>Informasi seputar Covid-19 di Indonesia</h1>
       <Carousel autoplay="true">
         <div>
-          <img src="https://www.internationalsos.co.id/-/media/indonesia-website/images/carousel/carousel--prokes-wide.jpg?h=450&la=en&mw=1280&w=1280&hash=68DC9C7962A3731D7FB3C4561C635DC3DEB1B823" style={contentStyle}/>
+          <img src="https://web.kominfo.go.id/sites/default/files/users/4896/Banner%20Bantu%20Masyarakat%20Tahu%20COVID-19.jpeg" style={contentStyle}/>
+        </div>
+        <div>
+          <img src="https://web.kominfo.go.id/sites/default/files/users/4896/Banner%20Bantu%20Masyarakat%20Tahu%20COVID-19.jpeg" style={contentStyle}/>
         </div>
         <div>
           <img src="https://web.kominfo.go.id/sites/default/files/users/4896/Banner%20Bantu%20Masyarakat%20Tahu%20COVID-19.jpeg" style={contentStyle}/>
